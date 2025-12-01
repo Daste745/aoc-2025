096b63b22c94bfd143a2f76f35ebdfe657852b04
Daste <stefankar1000@gmail.com>
2025-12-01 12:51:52 +0100

    day01: Cleanup

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,17 +1,29 @@
-use std::{fmt::Display, io, ops::Range, str::FromStr};
+use std::{fmt::Display, io, str::FromStr};
+
+#[derive(Debug, Clone)]
+enum Direction {
+    Left,
+    Right,
+}
+
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Left => write!(f, "L"),
+            Self::Right => write!(f, "R"),
+        }
+    }
+}
 
 #[derive(Debug)]
-enum Rotation {
-    Left { distance: i32 },
-    Right { distance: i32 },
+struct Rotation {
+    direction: Direction,
+    distance: i32,
 }
 
 impl Display for Rotation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Left { distance } => f.write_fmt(format_args!("L{distance}")),
-            Self::Right { distance } => f.write_fmt(format_args!("R{distance}")),
-        }
+        f.write_fmt(format_args!("{}{}", self.direction, self.distance))
     }
 }
 
@@ -20,15 +32,21 @@ impl FromStr for Rotation {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (dir, distance) = s.split_at(1);
+
+        let direction = match dir {
+            "L" => Direction::Left,
+            "R" => Direction::Right,
+            _ => return Err(format!("Unknown direction: {dir}")),
+        };
+
         let Ok(distance) = distance.parse::<i32>() else {
             return Err(format!("Failed to parse distance: {distance}"));
         };
 
-        match dir {
-            "L" => Ok(Self::Left { distance }),
-            "R" => Ok(Self::Right { distance }),
-            _ => Err(format!("Unknown direction: {dir}")),
-        }
+        Ok(Rotation {
+            direction,
+            distance,
+        })
     }
 }
 
@@ -49,13 +67,20 @@ impl Dial {
     }
 
     fn rotate(&mut self, rot: &Rotation) {
-        self.rotation = match rot {
-            Rotation::Left { distance } => {
-                let res = (self.rotation - distance) % 100;
+        self.rotation = match rot.direction {
+            Direction::Left => {
+                let res = (self.rotation - rot.distance) % 100;
                 if res >= 0 { res } else { 100 + res }
             }
-            Rotation::Right { distance } => (self.rotation + distance) % 100,
+            Direction::Right => (self.rotation + rot.distance) % 100,
         }
+    }
+
+    fn rotate_once(&mut self, direction: Direction) {
+        self.rotate(&Rotation {
+            direction,
+            distance: 1,
+        });
     }
 }
 
@@ -79,16 +104,10 @@ pub fn part2(input: &str) -> Result<String, io::Error> {
 
     let mut dial = Dial::new();
     let mut points_at_zero = 0;
+
     for rotation in rotations {
-        let amount = match rotation {
-            Rotation::Left { distance } => distance,
-            Rotation::Right { distance } => distance,
-        };
-        for _ in 0..amount {
-            dial.rotate(&match rotation {
-                Rotation::Left { distance: _ } => Rotation::Left { distance: 1 },
-                Rotation::Right { distance: _ } => Rotation::Right { distance: 1 },
-            });
+        for _ in 0..rotation.distance {
+            dial.rotate_once(rotation.direction.clone());
             if dial.rotation == 0 {
                 points_at_zero += 1;
             }
