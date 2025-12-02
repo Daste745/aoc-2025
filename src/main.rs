45aab94bc86bcceeeaa1e4a9c88b9dcf0dfad3fb
Daste <stefankar1000@gmail.com>
2025-12-02 12:40:51 +0100

    Use a macro to run days

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,31 @@ mod days;
 struct Cli {
     #[command(subcommand)]
     command: Command,
+
+    #[arg(short, long)]
+    input_path: String,
 }
 
 #[derive(Subcommand)]
 enum Command {
-    Day01 { input_path: String },
-    Day02 { input_path: String },
+    Day01,
+    Day02,
+}
+
+macro_rules! run_day {
+    ($module:ident, $input_path:expr) => {{
+        let input = read_to_string($input_path)?;
+        println!("Part 1: {}", days::$module::part1(&input)?);
+        println!("Part 2: {}", days::$module::part2(&input)?);
+    }};
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
-        Command::Day01 { input_path } => {
-            use days::day01::*;
-            let input = read_to_string(input_path)?;
-            println!("Part 1: {}", part1(&input)?);
-            println!("Part 2: {}", part2(&input)?);
-        }
-        Command::Day02 { input_path } => {
-            use days::day02::*;
-            let input = read_to_string(input_path)?;
-            println!("Part 1: {}", part1(&input)?);
-            println!("Part 2: {}", part2(&input)?);
-        }
+        Command::Day01 => run_day!(day01, cli.input_path),
+        Command::Day02 => run_day!(day02, cli.input_path),
     };
 
     Ok(())
