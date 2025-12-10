b6a21412b6a44cda3daf8ea3b45ba5d441f6d0e1
Daste <stefankar1000@gmail.com>
2025-12-10 21:48:55 +0100

    day05: Part 2

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,20 +1,109 @@
+use std::fmt::Display;
+
 use color_eyre::eyre::Result;
 
 use aoc_2025::aoc_tests;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Range {
     start: i64,
     end: i64,
 }
 
 impl Range {
+    #[allow(dead_code)]
+    fn new(start: i64, end: i64) -> Self {
+        Self { start, end }
+    }
+
     fn contains(&self, n: i64) -> bool {
         self.start <= n && n <= self.end
     }
 
     fn size(&self) -> i64 {
-        self.end - self.start
+        self.end - self.start + 1
+    }
+
+    fn fully_contains(&self, other: &Range) -> bool {
+        self.start <= other.start && other.end <= self.end
+    }
+
+    /// Find the difference between self and another Range.
+    ///
+    /// If the difference contains zero entries, return None.
+    ///
+    /// If the difference contains at least one entry, return a new Range.
+    fn difference(&self, other: &Range) -> Option<Range> {
+        if other.fully_contains(self) {
+            return None;
+        }
+
+        // Trim to be after the end of `other` if necessary
+        let start = if self.start > other.start && self.start <= other.end {
+            other.end + 1
+        } else {
+            self.start
+        };
+
+        // Trim to be before the start of `other` if necessary
+        let end = if self.end < other.end && self.end >= other.start {
+            other.start - 1
+        } else {
+            self.end
+        };
+
+        Some(Range { start, end })
+    }
+}
+
+impl Display for Range {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("({}..={})", self.start, self.end))
+    }
+}
+
+#[cfg(test)]
+mod range_tests {
+    use super::*;
+
+    /// (3..=7) - (8..=13) = (3..=7) (unchanged)
+    #[test]
+    fn difference_with_separate_end_and_start() {
+        let a = Range::new(3, 7);
+        let b = Range::new(8, 13);
+        assert_eq!(a.difference(&b), Some(Range::new(3, 7)));
+    }
+
+    /// (3..=7) - (7..=13) = (3..=6) (trimmed end)
+    #[test]
+    fn difference_with_overlapping_end_and_start() {
+        let a = Range::new(3, 7);
+        let b = Range::new(7, 13);
+        assert_eq!(a.difference(&b), Some(Range::new(3, 6)));
+    }
+
+    /// (7..=10) - (3..=6) = (7..=10) (unchanged)
+    #[test]
+    fn difference_with_separate_start_and_end() {
+        let a = Range::new(7, 10);
+        let b = Range::new(3, 6);
+        assert_eq!(a.difference(&b), Some(Range::new(7, 10)));
+    }
+
+    /// (7..=10) - (3..=7) = (8..=10) (trimmed start)
+    #[test]
+    fn difference_with_overlapping_start_and_end() {
+        let a = Range::new(7, 10);
+        let b = Range::new(3, 7);
+        assert_eq!(a.difference(&b), Some(Range::new(8, 10)));
+    }
+
+    /// (3..=7) - (7..=13) = None (fully contained in 2nd range)
+    #[test]
+    fn difference_with_full_overlap() {
+        let a = Range::new(3, 7);
+        let b = Range::new(3, 7);
+        assert_eq!(a.difference(&b), None);
     }
 }
 
@@ -62,17 +151,41 @@ pub fn part1(input: &str) -> Result<i64> {
 }
 
 pub fn part2(input: &str) -> Result<i64> {
-    let db = parse_input(input);
+    let mut db = parse_input(input);
 
-    // a -> 10..=100
-    // b -> 80..=150
-    // duplicated -> 100..=80
-    //
-    // HashSet -> too much results to remember
+    db.fresh.sort_by(|a, b| a.start.cmp(&b.start));
+    let ranges = db.fresh.iter().enumerate().filter_map(|(idx, range)| {
+        if db
+            .fresh
+            .iter()
+            .filter(|&other_range| other_range != range)
+            .any(|other_range| other_range.fully_contains(range))
+        {
+            println!("DROP\t{range}");
+            None
+        } else {
+            let combined = db
+                .fresh
+                .iter()
+                .enumerate()
+                .skip_while(|&(i, _)| i <= idx)
+                // .filter(|&(_, r)| r != range)
+                .fold(Some(*range), |acc, (_, r)| {
+                    if let Some(acc) = acc {
+                        let combined = acc.difference(r);
+                        println!("  DIFF\t{acc} - {r}\t-> {combined:?}");
+                        combined
+                    } else {
+                        println!("  DIFF -> None");
+                        None
+                    }
+                });
+            println!("COMBINE\t{range} -> {combined:?}");
+            combined
+        }
+    });
 
-    let result = db.fresh.iter().map(Range::size).sum();
-
-    Ok(result)
+    Ok(ranges.map(|range| range.size()).sum())
 }
 
 #[allow(dead_code)]
